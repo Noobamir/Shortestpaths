23c7d4531794a8f29682cd39435a5957fab6edaf
Noobamir <ca10g@my.fsu.edu>
2013-12-10 15:22:50 -0500

    Update shortestPaths.cpp

--- shortestPaths.cpp
+++ shortestPaths.cpp
@@ -1,4 +1,23 @@
-int main(){
-   return 0;
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Edge{
+   int weight;
+   int initialNode;
+   int terminalNode;
+};
+
+struct Graph{
+   bool direction;
+   vector<Edge> edgeList;
+};
+
+int main(int argc, char *argv[]){
+   char graphType = 'N';
+      char graphType = 'N';
+   return 0;     
 }
 
